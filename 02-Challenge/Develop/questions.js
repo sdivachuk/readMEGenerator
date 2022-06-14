b3f5d34df61b8b97eb5f3d9e09a641f2b8f9e2ff
Sergey Divachuk <sergeydivachuk@gmail.com>
2022-06-13 21:02:56 -0400

    added questions

---
+++ 02-Challenge/Develop/questions.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+      type: 'input', 
+      name: 'title',
+      message: 'What is the project title?',
+    },
+    {
+        type: 'input', 
+        name: 'description',
+        message: 'Write a description of the project',
+      },
+      {
+        type: 'input', 
+        name: 'installation',
+        message: 'What are the installation instructions?',
+      },
+      {
+        type: 'input', 
+        name: 'usage',
+        message: 'How do you use this project?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can someone contribute to this project?'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'How can someone test this project?',
+      }
+
+    ];
